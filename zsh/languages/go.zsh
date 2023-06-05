4548afb39b6479ad8b402fd1d447bc48a75e2ee3
James Cunningham <git@jamescun.com>
2023-06-05 15:16:24 +0100

    set GOPRIVATE for personal, CunninghamResearch, private Go modules

--- zsh/languages/go.zsh
+++ zsh/languages/go.zsh
@@ -2,6 +2,8 @@ if command_exists go; then
 	export GOPATH=$HOME/go:$HOME/projects
 	export PATH=$PATH:$HOME/go/bin:$HOME/projects/bin
 
+	export GOPRIVATE=github.com/jamescun/*,github.com/CunninghamResearch/*
+
 	alias got="go test -v -cover -coverprofile=.gocover"
 	alias goc="go tool cover -html=.gocover"
 	alias gob="go test -v -bench . -benchmem"
