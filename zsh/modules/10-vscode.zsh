7160c587d55a466f690bb904df79dd1ad1d8a4e7
James Cunningham <me@jamescun.com>
2026-06-06 12:08:55 +0100

    feat: add alias for VSCode open utility

---
+++ zsh/modules/10-vscode.zsh
@@ -0,0 +1,4 @@
+# if Visual Studio Code is installed, add it's aliases.
+if [ -d "/Applications/Visual Studio Code.app" ]; then
+	alias code="/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code"
+fi
