272817b2be2658b9c09d80b1cdc4e841603da476
James Cunningham <me@jamescun.com>
2026-06-16 09:19:17 +0100

    feat: add custom ZSH theme and plugins

--- zsh/modules/00-oh-my-zsh.zsh
+++ zsh/modules/00-oh-my-zsh.zsh
@@ -1,9 +1,12 @@
-# set the source for oh-my-zsh to vendored repository.
+# set the path to the vendored oh-my-zsh repository.
 export ZSH=$DOTFILES/zsh/vendor/ohmyzsh
 
-ZSH_THEME="robbyrussell"
+# set the path to the oh-my-zsh plugins.
+export ZSH_CUSTOM=$DOTFILES/zsh/ohmyzsh
+
+ZSH_THEME="jamescun"
 HYPHEN_INSENSITIVE="true"
 DISABLE_AUTO_UPDATE="true"
-plugins=(git)
+plugins=(git macos nats task)
 
 source $ZSH/oh-my-zsh.sh
